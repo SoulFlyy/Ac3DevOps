9ac164668644c8760f18a19787c6b4cccd6b5f2f
hecbale <hector.leite@hotmail.com>
2020-10-09 16:15:07 -0300

    testando erro de logica

--- t8.py
+++ t8.py
@@ -14,19 +14,19 @@ def nao_entre_em_panico():
 
     primos = "2,"
 
-    while p < limite:
-        ehprimo = 1
-        for i in range(2, numero):
-            if numero % i == 0:
-                ehprimo = 0
-                break
-        if ehprimo:
-            primos = primos + str(numero) + ','
-            p += 1
-            if(p % 10 == 0):
-                primos = primos + "\n"
-        numero += 1
-    return primos
+    # while p < limite:
+    #     ehprimo = 1
+    #     for i in range(2, numero):
+    #         if numero % i == 0:
+    #             ehprimo = 0
+    #             break
+    #     if ehprimo:
+    #         primos = primos + str(numero) + ','
+    #         p += 1
+    #         if(p % 10 == 0):
+    #             primos = primos + "\n"
+    #     numero += 1
+    # return primos
         
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 9000))
